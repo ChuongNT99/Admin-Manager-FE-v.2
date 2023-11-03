c853623842af4e5edda09f91361e4c253f1e19fa
Tiendat11 <ngothiendat258@gmail.com>
2023-11-03 10:17:11 +0700

    Base source Code Change

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,87 +1,15 @@
-import { useForm } from 'react-hook-form';
-import { EnvelopeFill, LockFill } from 'react-bootstrap-icons';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup';
-function BasicExample() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+import React from "react";
+import { Form, Input,Button,CheckBox } from 'antd';
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
-  return (
-    <Container>
-      <Row className="justify-content-center">
-        <Col xs={12} md={6}>
-          <h1 className="mt-5 mb-4 text-center">Form Example</h1>
-
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">
-                  <EnvelopeFill />
-                </InputGroup.Text>
-                <Form.Control
-                  type="email"
-                  value=""
-                  placeholder="Email"
-                  // aria-describedby="inputGroupPrepend"
-                  {...register('email', {
-                    required: 'Email is required',
-                    pattern: {
-                      value: /^\S+@\S+$/i,
-                      message: 'Invalid email format',
-                    },
-                  })}
-                  isInvalid={errors.email}
-                />
-                {errors.email && (
-                  <Form.Control.Feedback type="invalid">
-                    {errors.email.message}
-                  </Form.Control.Feedback>
-                )}
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">
-                  <LockFill />
-                </InputGroup.Text>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  value=""
-                  // aria-describedby="inputGroupPrepend"
-                  {...register('password', {
-                    required: 'Password is required',
-                  })}
-                  isInvalid={errors.password}
-                />
-                {errors.password && (
-                  <Form.Control.Feedback type="invalid">
-                    {errors.password.message}
-                  </Form.Control.Feedback>
-                )}
-              </InputGroup>
-            </Form.Group>
-
-            <Button variant="primary" type="submit" style={{width:"100%"}}>
-              Login
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
+const AppContact = () => {
+  return(
+    <div className="block contactBlock">
+      <div className="container-fluid">
+        <div className="titleHolder">
+          <h1>Form Login</h1>
+          <p>Welcome to Rikkeisoft</p>
+        </div>
+      </div>
+    </div>
+  )
 }
-
-export default BasicExample;
