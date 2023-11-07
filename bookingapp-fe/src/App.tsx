78c47b752e3d8bd3bbbe956fa707389dbec277d5
Tiendat11 <ngothiendat258@gmail.com>
2023-11-07 13:33:55 +0700

    dat

--- bookingapp-fe/src/App.tsx
+++ bookingapp-fe/src/App.tsx
@@ -1,18 +1,27 @@
-// import React from 'react';
-import './App.css';
-// import CreateRoom from './components/Rooms/CreateRoom';
-// import FormLogin  from './components/Form/Form';
-// import Home from './components/Rooms/Home';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Rooms/Home';
+// import BookingFormPage from './components/Booking/BookingFormPage';
+import CreateRoom from './components/Rooms/CreateRoom';
+import FormLogin from './components/Form/Form';
+import ProtectedRoute from './route/ProtectedRoute';
+import PrivateRoute from './route/PrivateRoute';
 
-
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
-    <>
-    {/* <FormLogin/> */}
-    {/* <Home/> */}
-    {/* <CreateRoom/> */}
-    </>
+      <div className="App">
+          <Routes>
+            <Route element = {<FormLogin/>} path='/login' />
+            <Route element={<ProtectedRoute/>} >
+                <Route element= {<PrivateRoute/>}>
+                  <Route element = { <Home/>}  path='/'   />
+                  <Route element = { <CreateRoom/> } path='/add' />
+                </Route>
+            </Route>
+
+           </Routes>
+      </div>
   );
-}
+};
 
 export default App;
--- bookingapp-fe/src/components/Rooms/Home.tsx
+++ bookingapp-fe/src/components/Rooms/Home.tsx
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Card, Button, Modal, Form, Alert, Input } from 'antd';
-// import { useNavigate } from 'react-router-dom';
-
+import { Card, Button, Modal, Alert, Form, Input } from 'antd';
+import BookingFormPage from '../Booking/BookingFormPage';
+import  { useSelector } from 'react-redux'
+import { RootState } from '../reducers/store';
 interface Room {
   room_id: number;
   room_name: string;
 }
 
+
 const Home: React.FC = () => {
   const [rooms, setRooms] = useState<Room[]>([]);
   const [roomName, setRoomName] = useState('');
-  const [editId, setEditId] = useState<number | null>(null);
-  const [showEditModal, setShowEditModal] = useState<boolean>(false);
   const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
+  const [showEditModal, setShowEditModal] = useState<boolean>(false);
   const [selectedRoom, setSelectedRoom] = useState<Room | null>(null);
   const [alertMessage, setAlertMessage] = useState('');
   const [isAlertVisible, setIsAlertVisible] = useState(false);
-  // const history = useNavigate();
+  const [showBookingForm, setShowBookingForm] = useState(false);
+  const token = useSelector((state: RootState ) => state.auth.token);
+  const token1 = localStorage.getItem('access_token');
 
-  const url: string = 'https://a71f-210-245-110-144.ngrok-free.app'; 
+
+  const url: string = 'https://a71f-210-245-110-144.ngrok-free.app';
 
   const fetchData = async () => {
     try {
@@ -32,6 +36,7 @@ const Home: React.FC = () => {
           'Ngrok-Trace-Id': 'bc47d5235e969cbcdd63082f9efdeb9c',
           Server: 'Werkzeug/3.0.0 Python/3.12.0',
           'cache-control': 'no-cache,private',
+          Authorization: `Bearer ${token1}`,
         },
       });
       setRooms(response.data.rooms);
@@ -43,35 +48,45 @@ const Home: React.FC = () => {
 
   useEffect(() => {
     fetchData();
-  }, []); 
-
-  const handleEdit = (id: number, name: string) => {
-    setEditId(id);
-    setRoomName(name);
-    setShowEditModal(true);
-  };
-
-  const handleUpdate = async () => {
-    try {
-      await axios.put(`${url}/rooms/${editId}`, { room_name: roomName });
-      fetchData();
-      setShowEditModal(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  }, []);
 
   const handleDelete = async () => {
     try {
       if (selectedRoom) {
-        await axios.delete(`${url}/rooms/${selectedRoom.room_id}`);
+        await axios.delete(`${url}/rooms/${selectedRoom.room_id}`,{
+          headers: {
+            Authorization: `Bearer ${token1}`
+          }
+        });
         fetchData();
         setShowDeleteModal(false);
         setAlertMessage('Xóa thành công!');
         setIsAlertVisible(true);
         setTimeout(() => {
           setIsAlertVisible(false);
-        }, 3000);
+        }, 1000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleEdit = (id: number, name: string) => {
+    setSelectedRoom({ room_id: id, room_name: name });
+    setRoomName(name);
+    setShowEditModal(true);
+  };
+
+  const handleUpdate = async () => {
+    try {
+      if (selectedRoom) {
+        await axios.put(`${url}/rooms/${selectedRoom.room_id}`, { room_name: roomName },{
+          headers:{
+            Authorization: `Bearer ${token1}`
+          }
+        });
+        fetchData();
+        setShowEditModal(false);
       }
     } catch (error) {
       console.log(error);
@@ -82,68 +97,35 @@ const Home: React.FC = () => {
     setShowEditModal(false);
   };
 
-  const handleToggleDeleteModal = (room: Room) => {
-    setSelectedRoom(room);
-    setShowDeleteModal(true);
-  };
-
   const handleCloseDeleteModal = () => {
     setShowDeleteModal(false);
   };
 
   const handleToggleBookingForm = (room: Room) => {
-    // history('/booking', { state: { selectedRoom: room } });
+    setSelectedRoom(room);
+    setShowBookingForm(true);
   };
-
   return (
+    
     <div className="container">
       {isAlertVisible && <Alert message={alertMessage} type="success" />}
       <div className="action">
-        <div className="card-group">
+        <div className="card-group" style={{display:"flex"}}>
           {rooms.map((room, key) => {
             return (
-              <Card key={key} className="room-card" style={{ margin: '10px', padding: '10px', border:"2px solid #dadada", borderRadius:'3rem' }}>
+              <Card key={key} className="room-card" style={{ width:"20%", height:"30%" ,margin: '10px', padding: '10px', border: '2px solid #dadada', borderRadius: '1rem' }}>
                 <Card.Meta
-                  title={
-                    editId === room.room_id ? (
-                      <Form>
-                        <Form.Item>
-                          <Input
-                            type="text"
-                            value={roomName}
-                            onChange={(e) => setRoomName(e.target.value)}
-                          />
-                        </Form.Item>
-                        <Form.Item>
-                          <Button type="primary" onClick={handleUpdate} style={{ marginRight: '5px' }}>
-                            Update
-                          </Button>
-                          <Button onClick={handleCloseEditModal}>Cancel</Button>
-                        </Form.Item>
-                      </Form>
-                    ) : (
-                      room.room_name
-                    )
-                  }
+                  title={<div>{room.room_name}</div>}
                   description={
-                    <div>
-                      {editId !== room.room_id && (
-                        <div>
-                          <Button onClick={() => handleEdit(room.room_id, room.room_name)} style={{ marginRight: '5px' }}>
-                            Edit
-                          </Button>
-                          <Button onClick={() => handleToggleBookingForm(room)}>
-                            Booking
-                          </Button>
-                          <Button danger onClick={() => handleToggleDeleteModal(room)}>
-                            Delete
-                          </Button>
-                        </div>
-                      )}
+                    <div style={{display:"flex", justifyContent:"space-around"}}>
+                      <Button onClick={() => handleToggleBookingForm(room)}>Booking</Button>
+                      <Button danger onClick={() => setShowDeleteModal(true)}>
+                        Delete
+                      </Button>
+                      <Button onClick={() => handleEdit(room.room_id, room.room_name)}>Edit</Button>
                     </div>
                   }
                 />
-                <Button onClick = {() => handleToggleBookingForm(room)}>Booking</Button>
               </Card>
             );
           })}
@@ -153,7 +135,7 @@ const Home: React.FC = () => {
       <Modal title="Edit Room" visible={showEditModal} onCancel={handleCloseEditModal} footer={null}>
         <Form>
           <Form.Item>
-            <Input type="text" value={roomName} onChange={(e:any) => setRoomName(e.target.value)} />
+            <Input type="text" value={roomName} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setRoomName(e.target.value)} />
           </Form.Item>
           <Form.Item>
             <Button type="primary" onClick={handleUpdate} style={{ marginRight: '5px' }}>
@@ -165,12 +147,16 @@ const Home: React.FC = () => {
       </Modal>
 
       <Modal title="Delete Room" visible={showDeleteModal} onCancel={handleCloseDeleteModal} footer={null}>
-        <p>Bạn có muốn xoá phòng này  "{selectedRoom?.room_name}" hay không ?</p>
+        <p>Bạn có muốn xoá phòng này "{selectedRoom?.room_name}" hay không?</p>
         <Button danger onClick={handleDelete} style={{ marginRight: '5px' }}>
           Delete
         </Button>
         <Button onClick={handleCloseDeleteModal}>Cancel</Button>
       </Modal>
+
+      {showBookingForm && (
+        <BookingFormPage selectedRoom={selectedRoom}  />
+      )}
     </div>
   );
 };
